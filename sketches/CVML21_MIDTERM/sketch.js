bbb5b963b0b0cdb5622ce87bc92ff1705ced0990
mweiss4 <mweiss4@saic.edu>
2021-03-08 21:40:31 -0600

    no idea what I'm doing anymore

--- sketches/CVML21_MIDTERM/sketch.js
+++ sketches/CVML21_MIDTERM/sketch.js
@@ -83,39 +83,39 @@ function setup() {
     textSize(11);
     textAlign(LEFT);
   
-    saveButton = c.createButton('Save');
+    saveButton = createButton('Save');
     saveButton.size(60, 25);
     saveButton.position(410, 375);
     saveButton.mousePressed(saveImg);
 
-    playButton = c.createButton('▶ / ❚❚');
+    playButton = createButton('▶ / ❚❚');
     playButton.size(60, 25);  
     playButton.position(410, 345);
     playButton.mousePressed(playPause);
   
-    clearButton = c.createButton('Clear');
+    clearButton = createButton('Clear');
     clearButton.size(60, 25);
     clearButton.position(475, 345);
     clearButton.mousePressed(clearStroke);
     
-    resetButton = c.createButton('Reset');
+    resetButton = createButton('Reset');
     resetButton.size(60, 25);
     resetButton.position(475, 375);
     resetButton.mousePressed(reset);
     
-    accSlider = c.createSlider(0.1, 0.5, 0.1, 0);
+    accSlider = createSlider(0.1, 0.5, 0.1, 0);
     accSlider.position(407, 15);
     accSlider.style('width', '100px');
   
-    strokeSliderMin = c.createSlider(0.5, 10, 0.5, 0);
+    strokeSliderMin = createSlider(0.5, 10, 0.5, 0);
     strokeSliderMin.position(407, 75);
     strokeSliderMin.style('width', '100px');
   
-    strokeSliderMax = c.createSlider(0.5, 10, 5, 0);
+    strokeSliderMax = createSlider(0.5, 10, 5, 0);
     strokeSliderMax.position(407, 95);
     strokeSliderMax.style('width', '100px');
   
-    segSlider = c.createSlider(50, 2400, 800, 50);
+    segSlider = createSlider(50, 2400, 800, 50);
     segSlider.position(407, 155);
     segSlider.style('width', '100px');
   
